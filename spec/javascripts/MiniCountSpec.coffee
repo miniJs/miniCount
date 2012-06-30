e3960c447a61d9d75ffd6e34ef885258c3bd376a
matthieu <matthieu.aussaguel@gmail.com>
2012-07-01 01:20:56 +1000

    Add more tests (WIP)

--- spec/javascripts/MiniCountSpec.coffee
+++ spec/javascripts/MiniCountSpec.coffee
@@ -3,8 +3,8 @@ describe 'miniCount', ->
     unit: 'word'
 
   beforeEach ->
-    loadFixtures 'fragment.html'
-    @$element = $('#fixtures')
+    setFixtures '<section><textarea></textarea></section>'    
+    @$element = $('textarea')
 
   it 'should be available on the jQuery object', ->
     expect($.fn.miniCount).toBeDefined()
@@ -20,3 +20,101 @@ describe 'miniCount', ->
   it 'should overwrites the settings', ->
     plugin = new $.miniCount(@$element[0], options)
     expect(plugin.settings.unit).toBe(options.unit)
+
+  describe 'counter element', ->
+    describe 'with default options', ->
+      beforeEach ->
+        @plugin = new $.miniCount(@$element)
+
+      it 'should add the counter after the element', ->
+        expect(@plugin.$counterWrapper).toBe(@$element.next())
+
+      it 'should be a div', -> 
+        expect(@$element.next().prop('tagName')).toBe('DIV')
+
+      it 'should have the default class name', ->
+        expect(@plugin.$counterWrapper.hasClass(@plugin.defaults.className)).toBeTruthy()
+
+      it 'should include the current count wrapped in a span', ->
+        expect(@plugin.$counterWrapper.children('span').html()).toBe '0'
+
+    describe 'custom options', ->
+      beforeEach ->
+        @plugin = new $.miniCount(@$element, {className: 'custom-class-name'})
+
+      it 'should have the default class name', ->
+        expect(@plugin.$counterWrapper.hasClass("custom-class-name")).toBeTruthy()
+
+  describe 'visual validation', ->
+    describe 'when valid', ->
+      it 'should add the custom text after the counter', ->
+        plugin = new $.miniCount(@$element, {text: 'some text'})
+        expect(plugin.$counter.next()).toBe(plugin.$text)
+
+      it 'should prepend the custom text', ->
+        plugin = new $.miniCount(@$element, {text: 'some text', textPosition: 'before' })
+        expect(plugin.$counter.prev()).toBe(plugin.$text)
+
+      it 'should include the custom text prepended by a space if position after', ->
+        plugin = new $.miniCount(@$element, {text: 'some text' })
+        expect(plugin.$text.text()).toBe(' some text')
+
+      it 'should include the custom text prepended by a space if position before', ->
+        plugin = new $.miniCount(@$element, {text: 'some text', textPosition: 'before' })
+        expect(plugin.$text.text()).toBe('some text ')
+
+    describe 'when invalid', ->
+      it 'should replace text invalid text when invalid', ->
+        plugin = new $.miniCount(@$element, {text: 'valid text', invalidText: 'invalid text', min: 5 })
+        expect(plugin.$text.text()).toBe(' invalid text')
+
+        @$element.val('this is now valid').trigger('change')
+        expect(plugin.$text.text()).toBe(' valid text')
+
+      it 'should add the default invalid class when text invalid', ->
+        plugin = new $.miniCount(@$element, {min: 5 })
+        expect(plugin.$counterWrapper.hasClass('error')).toBeTruthy()
+
+        @$element.val('this is now valid').trigger('change')
+        expect(plugin.$counterWrapper.hasClass('error')).toBeFalsy()
+
+      it 'should add the custom class when text invalid', ->
+        plugin = new $.miniCount(@$element, {min: 5, invalidClass: 'custom-invalid-class' })
+        expect(plugin.$counterWrapper.hasClass('custom-invalid-class')).toBeTruthy()
+
+        @$element.val('this is now valid').trigger('change')
+        expect(plugin.$counterWrapper.hasClass('custom-invalid-class')).toBeFalsy()
+
+  describe 'character count', ->
+
+
+  describe 'word count', ->
+
+  describe 'sentence count', ->
+
+  describe 'hideOnValid', ->
+
+  describe 'countdown', ->
+
+  describe 'callbacks', ->
+    beforeEach ->
+      @foo = jasmine.createSpy('foo')
+
+    it 'should call valid callback function when valid', ->
+      plugin  = new $.miniCount(@$element, { min: 5, onValid: @foo})
+      expect(@foo).not.toHaveBeenCalled()
+      @$element.val('this is now valid').trigger('change')
+      expect(@foo).toHaveBeenCalled()
+
+    it 'should call invalid callback function when invalid', ->
+      plugin  = new $.miniCount(@$element, { max: 5, onInvalid: @foo })
+      expect(@foo).not.toHaveBeenCalled()
+      @$element.val('this is now invalid').trigger('change')
+      expect(@foo).toHaveBeenCalled()
+
+
+
+
+
+
+
--- spec/javascripts/MiniCountSpec.js
+++ spec/javascripts/MiniCountSpec.js
@@ -6,8 +6,8 @@
       unit: 'word'
     };
     beforeEach(function() {
-      loadFixtures('fragment.html');
-      return this.$element = $('#fixtures');
+      setFixtures('<section><textarea></textarea></section>');
+      return this.$element = $('textarea');
     });
     it('should be available on the jQuery object', function() {
       return expect($.fn.miniCount).toBeDefined();
@@ -20,10 +20,135 @@
       plugin = new $.miniCount(this.$element[0], options);
       return expect(plugin.defaults).toBeDefined();
     });
-    return it('should overwrites the settings', function() {
+    it('should overwrites the settings', function() {
       var plugin;
       plugin = new $.miniCount(this.$element[0], options);
       return expect(plugin.settings.unit).toBe(options.unit);
+    });
+    describe('counter element', function() {
+      describe('with default options', function() {
+        beforeEach(function() {
+          return this.plugin = new $.miniCount(this.$element);
+        });
+        it('should add the counter after the element', function() {
+          return expect(this.plugin.$counterWrapper).toBe(this.$element.next());
+        });
+        it('should be a div', function() {
+          return expect(this.$element.next().prop('tagName')).toBe('DIV');
+        });
+        it('should have the default class name', function() {
+          return expect(this.plugin.$counterWrapper.hasClass(this.plugin.defaults.className)).toBeTruthy();
+        });
+        return it('should include the current count wrapped in a span', function() {
+          return expect(this.plugin.$counterWrapper.children('span').html()).toBe('0');
+        });
+      });
+      return describe('custom options', function() {
+        beforeEach(function() {
+          return this.plugin = new $.miniCount(this.$element, {
+            className: 'custom-class-name'
+          });
+        });
+        return it('should have the default class name', function() {
+          return expect(this.plugin.$counterWrapper.hasClass("custom-class-name")).toBeTruthy();
+        });
+      });
+    });
+    describe('visual validation', function() {
+      describe('when valid', function() {
+        it('should add the custom text after the counter', function() {
+          var plugin;
+          plugin = new $.miniCount(this.$element, {
+            text: 'some text'
+          });
+          return expect(plugin.$counter.next()).toBe(plugin.$text);
+        });
+        it('should prepend the custom text', function() {
+          var plugin;
+          plugin = new $.miniCount(this.$element, {
+            text: 'some text',
+            textPosition: 'before'
+          });
+          return expect(plugin.$counter.prev()).toBe(plugin.$text);
+        });
+        it('should include the custom text prepended by a space if position after', function() {
+          var plugin;
+          plugin = new $.miniCount(this.$element, {
+            text: 'some text'
+          });
+          return expect(plugin.$text.text()).toBe(' some text');
+        });
+        return it('should include the custom text prepended by a space if position before', function() {
+          var plugin;
+          plugin = new $.miniCount(this.$element, {
+            text: 'some text',
+            textPosition: 'before'
+          });
+          return expect(plugin.$text.text()).toBe('some text ');
+        });
+      });
+      return describe('when invalid', function() {
+        it('should replace text invalid text when invalid', function() {
+          var plugin;
+          plugin = new $.miniCount(this.$element, {
+            text: 'valid text',
+            invalidText: 'invalid text',
+            min: 5
+          });
+          expect(plugin.$text.text()).toBe(' invalid text');
+          this.$element.val('this is now valid').trigger('change');
+          return expect(plugin.$text.text()).toBe(' valid text');
+        });
+        it('should add the default invalid class when text invalid', function() {
+          var plugin;
+          plugin = new $.miniCount(this.$element, {
+            min: 5
+          });
+          expect(plugin.$counterWrapper.hasClass('error')).toBeTruthy();
+          this.$element.val('this is now valid').trigger('change');
+          return expect(plugin.$counterWrapper.hasClass('error')).toBeFalsy();
+        });
+        return it('should add the custom class when text invalid', function() {
+          var plugin;
+          plugin = new $.miniCount(this.$element, {
+            min: 5,
+            invalidClass: 'custom-invalid-class'
+          });
+          expect(plugin.$counterWrapper.hasClass('custom-invalid-class')).toBeTruthy();
+          this.$element.val('this is now valid').trigger('change');
+          return expect(plugin.$counterWrapper.hasClass('custom-invalid-class')).toBeFalsy();
+        });
+      });
+    });
+    describe('character count', function() {});
+    describe('word count', function() {});
+    describe('sentence count', function() {});
+    describe('hideOnValid', function() {});
+    describe('countdown', function() {});
+    return describe('callbacks', function() {
+      beforeEach(function() {
+        return this.foo = jasmine.createSpy('foo');
+      });
+      it('should call valid callback function when valid', function() {
+        var plugin;
+        plugin = new $.miniCount(this.$element, {
+          min: 5,
+          onValid: this.foo
+        });
+        expect(this.foo).not.toHaveBeenCalled();
+        this.$element.val('this is now valid').trigger('change');
+        return expect(this.foo).toHaveBeenCalled();
+      });
+      return it('should call invalid callback function when invalid', function() {
+        var plugin;
+        plugin = new $.miniCount(this.$element, {
+          max: 5,
+          onInvalid: this.foo
+        });
+        expect(this.foo).not.toHaveBeenCalled();
+        this.$element.val('this is now invalid').trigger('change');
+        return expect(this.foo).toHaveBeenCalled();
+      });
     });
   });
 
